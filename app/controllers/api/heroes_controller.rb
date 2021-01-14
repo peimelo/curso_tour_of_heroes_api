0a5e31dc6858fcc070d88d5d03592ac63a53a4f7
Paulo Melo <peimelo@gmail.com>
2021-01-14 20:07:58 -0300

    Adiciona busca na lista de heróis

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -3,7 +3,7 @@ class Api::HeroesController < ApplicationController
 
   # GET /heroes
   def index
-    @heroes = Hero.all.sorted_by_name
+    @heroes = Hero.search(params[:term]).sorted_by_name
 
     render json: @heroes
   end
