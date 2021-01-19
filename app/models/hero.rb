2c9b0f83bbc5addf3526b9aa93a3959be36a671c
Paulo Melo <peimelo@gmail.com>
2021-01-19 07:05:43 -0300

    Altera CRUD para token no banco de dados

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,7 @@
 class Hero < ApplicationRecord
   validates :name, :token, presence: true
 
+  scope :by_token, ->(token) { where(token: token) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
   scope :sorted_by_name, -> { order(:name) }
 end
