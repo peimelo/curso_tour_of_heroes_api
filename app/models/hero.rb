60dc9fe4694dce376c76a6294566d64e7b55202f
Paulo Melo <peimelo@gmail.com>
2021-02-02 19:37:19 -0300

    Adiciona testes no model Hero

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,8 +1,8 @@
 class Hero < ApplicationRecord
-  validates :name, :token, presence: true
-  validates :name, uniqueness: { scope: :token, case_sensitive: false }
+  validates :name, presence: true, uniqueness: { scope: :token, case_sensitive: false }
+  validates :token, presence: true, length: { minimum: 10 }
 
   scope :by_token, ->(token) { where(token: token) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
-  scope :sorted_by_name, -> { order(:name) }
+  scope :sorted_by_name, -> { order(name: :desc) }
 end
